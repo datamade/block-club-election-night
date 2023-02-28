7e3784142d06a3df662692633cadc6a99d5aed4c
hannah cushman garland <hannah.cushman@gmail.com>
2023-02-28 17:06:32 -0600

    Add flag to run test scrape

--- election_night/get_results.py
+++ election_night/get_results.py
@@ -5,82 +5,111 @@ import re
 
 from chi_elections import SummaryClient
 
+if __name__ == "__main__":
+    import argparse
+    import sys
 
-client = SummaryClient(url="https://chicagoelections.gov/results/ap/SummaryExport.txt")
-client.fetch()
+    parser = argparse.ArgumentParser(
+        description="Scrape election results"
+    )
+    parser.add_argument(
+        "--test",
+        help="Pass this flag to scrape results from the test URL",
+        action="store_true",
+        default=False,
+    )
 
-output_directory = os.path.join(
-    os.getcwd(),
-    "results",
-    datetime.datetime.now().isoformat(),
-)
+    options = parser.parse_args()
 
-os.mkdir(output_directory)
+    if options.test:
+        summary_url = "https://chicagoelections.gov/results/ap/SummaryExport.txt"
+    else:
+        summary_url = "https://chicagoelections.gov/ap/SummaryExport.txt"
 
-police_district_results = []
-referendum_results = []
+    client = SummaryClient(url=summary_url)
+    client.fetch()
 
-for race in client.races:
-    if race.name == "Totals":
-        print(race.serialize())
-        continue
+    print(len(client.races))
 
-    race_name = re.sub(r"(Council Member,|Alderperson|Chicago Police Department)\s", "", race.name)
+    if len(client.races) == 1:
+        print(f"No results at {summary_url} yet. Please try again after 7 p.m. on Election Night. 🗳")
+        sys.exit()
 
-    if race.reporting_unit_name == "POLICE":
-        race_name = f"{race_name} Council"
+    else:
+        print(f"Getting results from {summary_url}! 🎉")
 
-    race_results = []
+    output_directory = os.path.join(
+        os.getcwd(),
+        "results",
+        datetime.datetime.now().isoformat(),
+    )
 
-    for candidate in race.candidates:
-        if race.reporting_unit_name != "SPECIAL REFERENDUM":
-            choice_name = candidate.full_name.title()
+    os.mkdir(output_directory)
 
-            if choice_name == "Cb Johnson":
-                choice_name = "CB Johnson"
-        else:
-            choice_name = candidate.full_name
+    police_district_results = []
+    referendum_results = []
 
-        row = [
-            race_name,
-            choice_name,
-            candidate.vote_total,
-            candidate.vote_total / (race.total_ballots_cast or 1) * 100,
-            race.precincts_reporting,
-            race.precincts_total,
-        ]
+    for race in client.races:
+        if race.name == "Totals":
+            print(race.serialize())
+            continue
+
+        race_name = re.sub(r"(Council Member,|Alderperson|Chicago Police Department)\s", "", race.name)
 
         if race.reporting_unit_name == "POLICE":
-            police_district_results.append(row)
-        elif race.reporting_unit_name == "SPECIAL REFERENDUM":
-            referendum_results.append(row)
-        else:
-            race_results.append(row)
+            race_name = f"{race_name} Council"
 
-    if race.reporting_unit_name not in ("POLICE", "SPECIAL REFERENDUM"):
-        with open(os.path.join(output_directory, f"{race_name}.csv"), "w") as output_file:
+        race_results = []
+
+        for candidate in race.candidates:
+            if race.reporting_unit_name != "SPECIAL REFERENDUM":
+                choice_name = candidate.full_name.title()
+
+                if choice_name == "Cb Johnson":
+                    choice_name = "CB Johnson"
+            else:
+                choice_name = candidate.full_name
+
+            row = [
+                race_name,
+                choice_name,
+                candidate.vote_total,
+                candidate.vote_total / (race.total_ballots_cast or 1) * 100,
+                race.precincts_reporting,
+                race.precincts_total,
+            ]
+
+            if race.reporting_unit_name == "POLICE":
+                police_district_results.append(row)
+            elif race.reporting_unit_name == "SPECIAL REFERENDUM":
+                referendum_results.append(row)
+            else:
+                race_results.append(row)
+
+        if race.reporting_unit_name not in ("POLICE", "SPECIAL REFERENDUM"):
+            with open(os.path.join(output_directory, f"{race_name}.csv"), "w") as output_file:
+                writer = csv.writer(output_file)
+                writer.writerow(["Candidate", "Votes Total", "Votes Percent"])
+                writer.writerows(row[1:4] + [""] for row in race_results)
+                precincts_reporting = race_results[0][-2]
+                precincts_total = race_results[0][-1]
+                writer.writerow([
+                    f"{precincts_reporting} of {precincts_total} precincts reporting", "", "",
+                ])
+
+
+    for file, contents, first_column in (
+        ("Police Councils.csv", police_district_results, "District"),
+        ("Misc.csv", referendum_results, "Question")
+    ):
+        with open(os.path.join(output_directory, file), "w") as output_file:
             writer = csv.writer(output_file)
-            writer.writerow(["Candidate", "Votes Total", "Votes Percent"])
-            writer.writerows(row[1:4] + [""] for row in race_results)
-            precincts_reporting = race_results[0][-2]
-            precincts_total = race_results[0][-1]
             writer.writerow([
-                f"{precincts_reporting} of {precincts_total} precincts reporting", "", "",
+                first_column,
+                "Candidate",
+                "Votes Total",
+                "Votes Percent",
+                "Precincts Reporting",
+                "Precincts Total"
             ])
-
-
-for file, contents, first_column in (
-    ("Police Councils.csv", police_district_results, "District"),
-    ("Misc.csv", referendum_results, "Question")
-):
-    with open(os.path.join(output_directory, file), "w") as output_file:
-        writer = csv.writer(output_file)
-        writer.writerow([
-            first_column,
-            "Candidate",
-            "Votes Total",
-            "Votes Percent",
-            "Precincts Reporting",
-            "Precincts Total"
-        ])
-        writer.writerows(contents)
+            writer.writerows(contents)
